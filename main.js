c0c3d62a28c36015057205e7f44d6ace026ca25a
noname068218 <128069790+noname068218@users.noreply.github.com>
2023-06-09 22:46:19 +0200

    end

---
+++ main.js
@@ -0,0 +1,7 @@
+import './js/buy-now-modal';
+import './js/swipper';
+import './js/subscribe-modal';
+import './js/slow-scroll';
+import './js/modal';
+import './js/anchor-scroll';
+import './js/modal-leave-review';
